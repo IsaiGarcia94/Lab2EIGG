a5092d1d1fc9c27571505e385fadc00702733a9f
IsaiGarcia94 <93231501+IsaiGarcia94@users.noreply.github.com>
2022-04-23 18:52:22 -0600

    Se agregar el programa

---
+++ app/Models/Productos.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+class Productos extends Eloquent
+{
+    //
+    protected $connection = 'mongodb';
+    protected $collection = 'productos';
+    protected $fillable = ["codprod", "nombreprod","descriprod", "typprod", "costunit", "cantprod"];
+   
+
+}
+
